aeb1650b6ad6d4ca75e449c6cbd058e51bce155f
Ferminju <ferminjuliancruz26@gmail.com>
2025-09-04 09:47:34 -0300

    free-trial trustindex

--- src/components/home/TrustindexWidget.tsx
+++ src/components/home/TrustindexWidget.tsx
@@ -2,7 +2,7 @@ export default function TrustindexWidget() {
     return (
         <div className="w-full mx-auto">
             <iframe
-                src="https://cdn.trustindex.io/amp-widget.html#e9bf221537071062990685f8f29"
+                src="https://cdn.trustindex.io/amp-widget.html#a9930da53c9c4720552696470d2"
                 className="w-full h-auto min-h-[300px]"
                 sandbox="allow-scripts allow-same-origin"
                 scrolling="no"
